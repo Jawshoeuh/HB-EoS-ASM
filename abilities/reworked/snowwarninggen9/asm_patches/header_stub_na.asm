bdf2185c63571d6160c72d4a9c1407660340bfd0
Josucschua <joshua.stefan.pang@gmail.com>
2022-12-02 02:18:53 -0800

    Removed work on snowwarning skypatch
    Removed groundwork for snowwarning sky patch, will reinclude when packaged into a skypatch format.

--- abilities/reworked/snowwarninggen9/asm_patches/header_stub_na.asm
+++
@@ -1,5 +0,0 @@
-.relativeinclude on
-.nds
-.arm
-
-.definelabel SnowWarningChangeWeather,      0x022F93BC
--- abilities/reworked/snowwarninggen9/asm_patches/patch.asm
+++
@@ -1,8 +0,0 @@
-;-------------------------------------
-; Replaces the top screen background
-; with one of the others.
-;-------------------------------------
-.org StringTopBg
-.area 0x4
-    strhne r5,[r0,0x58]
-.endarea
