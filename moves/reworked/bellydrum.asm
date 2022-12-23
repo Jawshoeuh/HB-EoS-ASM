80d35a0c761f3fbf91c2d2944f196750a7016ea6
Josucschua <joshua.stefan.pang@gmail.com>
2022-12-22 16:25:53 -0800

    Fixed unintended bug in filleyaway1 and bellydrum
    Previous versions accidently did more damage if the user had the ability Reckless (ability ID # 92 (0x5C)

--- moves/reworked/bellydrum.asm
+++ moves/reworked/bellydrum.asm
@@ -19,12 +19,14 @@
 .include "lib/dunlib_us.asm"
 .definelabel MoveStartAddress, 0x02330134
 .definelabel MoveJumpAddress, 0x023326CC
+.definelabel UpdateStatusIconFlags, 0x022E3AB4
 
 ; For EU
 ;.include "lib/stdlib_eu.asm"
 ;.include "lib/dunlib_eu.asm"
 ;.definelabel MoveStartAddress, 0x02330B74
 ;.definelabel MoveJumpAddress, 0x0233310C
+;.definelabel UpdateStatusIconFlags, 0x022E4464
 
 ; File creation
 .create "./code_out.bin", 0x02330134 ; Change to the actual offset as this directive doesn't accept labels
@@ -32,22 +34,34 @@
     .area MaxSize ; Define the size of the area
         
         ; Calculate Health
-        ldr   r0,[r9,#0xb4]
+        ldr   r0,[r4,#0xb4]
         ldrsh r1,[r0,#0x12]
         ldrsh r2,[r0,#0x16]
-        ldrsh r0,[r0,#0x10] ; Current HP
-        add r1,r1,r2        ; Max HP
+        ldrsh r3,[r0,#0x10]   ; Current HP
+        add   r1,r1,r2        ; Max HP
         
-        ; Check if recoil higher than current health.
-        lsr r1,r1,#1 ; Max HP / 2
-        cmp r1,r0
-        bge failed_recoil
+        ; Check if health cut is too much.
+        lsr  r1,r1,#1 ; Max HP / 2
+        subs r3,r3,r1 ; Do math and compare at same time.
+        bgt  success
         
-        ; Damage self
-        mov r0,r9
+        ; FAILED, HEALTH TOO LOW
+        mov r0,#0
+        mov r1,r4
         mov r2,#0
-        mov r3,#0
-        bl ConstDamage
+        bl  ChangeString ; User
+        mov r0,r4
+        ldr r1,=bellydrum_fail_str
+        bl  SendMessageWithStringLog
+        
+        mov r10,#0
+        b MoveJumpAddress
+        
+    success:
+        ; Simply set our health lower and update.
+        strh r3,[r0,#0x10]
+        mov r0,r4
+        bl UpdateStatusIconFlags
         
         ; SKYROCKET attack
         mov r0,r9
@@ -56,23 +70,20 @@
         mov r3,#0x63
         bl AttackStatUp
         
-        mov r10,#1
-        b MoveJumpAddress
-        
-    failed_recoil:
         mov r0,#0
         mov r1,r4
         mov r2,#0
         bl  ChangeString ; User
         mov r0,r4
-        ldr r1,=bellydrum_str
+        ldr r1,=bellydrum_pass_str
         bl  SendMessageWithStringLog
         
-        mov r10,#0
-        ; Always branch at the end
+        mov r10,#1
         b MoveJumpAddress
         .pool
-    bellydrum_str:
+    bellydrum_pass_str:
+        .asciiz "[string:0] cut its health by half!" 
+    bellydrum_fail_str:
         .asciiz "But [string:0] didn't have enough health!" 
     .endarea
 .close
